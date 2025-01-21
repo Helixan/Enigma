fbb16149fafa136487b4f8c296aa36b08b3ae317
Helixan <helixan@proton.me>
2025-01-20 20:35:48 -0500

    Created a database manager

---
+++ src/core/dbmanager.cpp
@@ -0,0 +1,40 @@
+#include "dbmanager.h"
+#include <QDebug>
+
+DBManager& DBManager::instance()
+{
+    static DBManager instance;
+    return instance;
+}
+
+bool DBManager::openConnection(const QString& host, const QString& dbName, const QString& user, const QString& password)
+{
+    db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName(host);
+    db.setDatabaseName(dbName);
+    db.setUserName(user);
+    db.setPassword(password);
+
+    if (!db.open()) {
+        qDebug() << "Database Error:" << db.lastError().text();
+        return false;
+    }
+    return true;
+}
+
+QSqlDatabase DBManager::getDatabase()
+{
+    return db;
+}
+
+void DBManager::closeConnection()
+{
+    if (db.isOpen()) {
+        db.close();
+    }
+}
+
+DBManager::~DBManager()
+{
+    closeConnection();
+}
---
+++ src/core/dbmanager.h
@@ -0,0 +1,23 @@
+#ifndef DBMANAGER_H
+#define DBMANAGER_H
+
+#include <QtSql>
+
+class DBManager
+{
+public:
+    static DBManager& instance();
+    bool openConnection(const QString& host, const QString& dbName, const QString& user, const QString& password);
+    QSqlDatabase getDatabase();
+    void closeConnection();
+
+private:
+    DBManager() {}
+    ~DBManager();
+    DBManager(const DBManager&) = delete;
+    DBManager& operator=(const DBManager&) = delete;
+
+    QSqlDatabase db;
+};
+
+#endif // DBMANAGER_H
